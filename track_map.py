7c208027f012b17e38f02e25ff75c26659ffbd3e
Giannis <giortzis.giannis@gmail.com>
2020-01-18 22:47:40 +0200

    trackmap

---
+++ track_map.py
@@ -0,0 +1,21 @@
+# importing libraries
+from kivy.lang import Builder
+from kivy.uix.floatlayout import FloatLayout
+from kivy.graphics import Rectangle, Color, Canvas, Line
+from kivy.uix.label import Label
+from kivy.properties import NumericProperty, ListProperty, ColorProperty, StringProperty, BoundedNumericProperty
+
+Builder.load_string("""
+<Track_map>:
+    canvas:
+        Color:
+            rgb: 0.1,0.1,0.1
+        Rectangle:
+            pos: self.pos
+            size: self.size
+            source: "./assets/Tracklayout.png"
+""")
+
+class Track_map(FloatLayout):
+    def __init__(self, **kwargs):
+        super(Track_map, self).__init__(**kwargs)
